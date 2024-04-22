1ccde0d75ff7e9d96fe97cdb46dc778f7bcc198d
Max Denisenko <maksim.denisenko.20@mail.ru>
2024-04-22 11:23:57 +0300

    использован цикл for

--- lec_04_3_array/lec_04_3_array.cpp
+++ lec_04_3_array/lec_04_3_array.cpp
@@ -4,6 +4,14 @@
 int main()
 {
     int A[10];
+    *A = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        int* p = A + i;
+        A[i] = i;
+        std::cout << *A << "\n" << p << "\n";
+        *A = *A + 1;
+    }
     A[0] = 5;
     int* p = A;
     *p = 10;

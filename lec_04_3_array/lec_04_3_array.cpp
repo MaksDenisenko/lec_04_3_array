23ab99d972e9372cd958171b088506b12ea53e95
Max Denisenko <maksim.denisenko.20@mail.ru>
2024-04-22 11:13:54 +0300

    write in A[1]

--- lec_04_3_array/lec_04_3_array.cpp
+++ lec_04_3_array/lec_04_3_array.cpp
@@ -5,8 +5,12 @@ int main()
 {
     int A[10];
     A[0] = 5;
-    int* p = &(A[0]);
+    int* p = A;
+    *p = 10;
 
-    std::cout << A[0] << "\n " << p;
+    std::cout << A[0] << "\n" << p << "\n";
+    p = p + 1;
+    *p = 20;
+    std::cout << A[1] << "\n" << p << "\n";
 }
 
